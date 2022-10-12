5dd3d59fe956d7efbb8f1e5152c3795059eddbe6
James Levell <jimmy.levell@outlook.com>
2022-10-12 14:56:54 +0200

    Feature: improved production entrypoint script

--- backend/config/config.js
+++ backend/config/config.js
@@ -6,14 +6,16 @@ module.exports = {
     "password": process.env.DB_PASSWORD || 'password',
     "database": process.env.DB_NAME || 'database_development',
     "host": process.env.DB_HOST || '127.0.0.1',
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "seederStorage": 'sequelize'
   },
   "test": {
     "username": process.env.DB_USER || 'postgres',
     "password": process.env.DB_PASSWORD || 'password',
     "database": process.env.DB_NAME || 'database_test',
     "host": process.env.DB_HOST || '127.0.0.1',
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "seederStorage": 'sequelize'
   },
   "production": {
     "username": process.env.DB_USER || 'postgres',
--- backend/entrypoints/production.sh
+++ backend/entrypoints/production.sh
@@ -3,6 +3,9 @@ set -e
 
 echo "WAITING FOR DATABASE. DB_HOST = $DB_HOST"
 ./entrypoints/wait-for.sh $DB_HOST:5432
+
+npx sequelize-cli db:create || true
 npx sequelize-cli db:migrate --migrations-path src/migrations || true
+npx sequelize-cli db:seed:all --seeders-path src/seeders || true
 
 exec "$@"
